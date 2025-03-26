2b6b1b450accd804fb7a8c7588ac207973ecdf17
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-26 18:29:27 +0900

    init

---
+++ src/main/java/com/invy/backend/InvyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.invy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invy/backend/InvyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
